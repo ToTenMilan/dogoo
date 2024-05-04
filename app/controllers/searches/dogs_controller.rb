4fbb0ebf73b1fba9eb314ea384c43f0074f81b84
Grzegorz Milanowski <grzegorz.milanowski@zeneducate.com>
2024-05-04 18:40:20 +0200

    handle searches dogs form with turbo

--- app/controllers/searches/dogs_controller.rb
+++ app/controllers/searches/dogs_controller.rb
@@ -1,15 +1,18 @@
 class Searches::DogsController < ApplicationController
   def index
-    @breed = params[:breed]
-    # data = ::DogApi::FetchDogImageByBreed.new(@breed)
+    @breed = params[:breed] || 'rottweiler'
+
     data = DogApi.random_dog_image_by_breed(@breed)
 
     @image_url = data[:image_url]
     @status = data[:status]
 
     respond_to do |format|
+      # FORM is not processed as TURBO_Stream format
+
+
+      format.turbo_stream { render turbo_stream: turbo_stream.replace('breed_image_result', partial: 'searches/dogs/result' ) }
       format.html { render :index }
-      format.turbo_stream { render turbo_stream: turbo_stream.replace('breed_image_result', partial: 'searches_dog_result' ) }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :searches do
-    resources :dogs, only: [:index, :show]
+    resources :dogs, only: [:index]
+    post :dogs, to: "dogs#index"
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
