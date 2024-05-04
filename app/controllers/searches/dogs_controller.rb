70276d37febb2f1d8925dcf392775f156c7f3865
Grzegorz Milanowski <grzegorz.milanowski@zeneducate.com>
2024-05-04 19:03:56 +0200

    cleanup

--- app/controllers/searches/dogs_controller.rb
+++ app/controllers/searches/dogs_controller.rb
@@ -1,18 +1,19 @@
 class Searches::DogsController < ApplicationController
   def index
-    @breed = params[:breed] || 'rottweiler'
+    @breed = params[:breed]
+# binding.pry
+    if @breed.present?
+      data = DogApi.random_dog_image_by_breed(@breed)
 
-    data = DogApi.random_dog_image_by_breed(@breed)
+      @image_url = data[:image_url]
+      @status = data[:status]
 
-    @image_url = data[:image_url]
-    @status = data[:status]
-
-    respond_to do |format|
-      # FORM is not processed as TURBO_Stream format
-
-
-      format.turbo_stream { render turbo_stream: turbo_stream.replace('breed_image_result', partial: 'searches/dogs/result' ) }
-      format.html { render :index }
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('breed_image_result', partial: 'searches/dogs/result' ) }
+        format.html { render :index }
+      end
+    else
+      render :index
     end
   end
 end
